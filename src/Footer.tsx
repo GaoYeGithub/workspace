5808963157e1228f810fbee8d297e01583fb5df7
gaoyegithub <152664000+GaoYeGithub@users.noreply.github.com>
2024-07-15 23:08:08 +0000

    React

---
+++ src/Footer.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <small>© 2021 Ziroll development. All rights reserved.</small>
+    </footer>
+  );
+}
---
+++ src/Navbar.tsx
@@ -0,0 +1,6 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+export default function Main() {
+  return <h1>Main component</h1>;
+}
